7a3f9c23fb688e0d3d6117c0cd18cdad30f83be6
lasconic <lasconic@gmail.com>
2014-08-17 18:35:51 +0200

    fix path in zip file

--- tx2s3.py
+++ tx2s3.py
@@ -44,6 +44,7 @@ def processTsFile(prefix, langCode, data):
 
 		return True
 	else:
+		print prefix + ' ' + lang_code + " not changed"
 		return False
 
 
@@ -88,9 +89,9 @@ for lang_code, languageName in langCodeNameDict.iteritems():
 		zipPath = outputDir + zipName
 		myzip = zipfile.ZipFile(zipPath, mode='w')
 		qmFilePath = outputDir + 'mscore_' + lang_code + ".qm"
-		myzip.write(qmFilePath)
+		myzip.write(qmFilePath, 'mscore_' + lang_code + ".qm")
 		qmFilePath = outputDir + 'instruments_' + lang_code + ".qm"
-		myzip.write(qmFilePath)
+		myzip.write(qmFilePath, 'instruments_' + lang_code + ".qm")
 		myzip.close()
 
 		# get zip file size
@@ -108,8 +109,8 @@ for lang_code, languageName in langCodeNameDict.iteritems():
 		data[lang_code]["hash"] = str(hash_file.hexdigest())
 		data[lang_code]["file_size"] = file_size
 
-		push_zip = subprocess.Popen(['s3cmd','put','--acl-public', '--guess-mime-type', zipPath, s3Url + zipName])
-		push_zip.communicate()
+		#push_zip = subprocess.Popen(['s3cmd','put','--acl-public', '--guess-mime-type', zipPath, s3Url + zipName])
+		#push_zip.communicate()
 
 
 
@@ -117,8 +118,8 @@ json_file = open(outputDir + "details.json", "w")
 json_file.write(json.dumps(data, sort_keys=True, indent=4))
 json_file.close()
 
-if translationChanged:
-	push_json = subprocess.Popen(['s3cmd','put','--acl-public', '--guess-mime-type', outputDir + 'details.json', s3Url + 'details.json'])
-	push_json.communicate()
+#if translationChanged:
+	#push_json = subprocess.Popen(['s3cmd','put','--acl-public', '--guess-mime-type', outputDir + 'details.json', s3Url + 'details.json'])
+	#push_json.communicate()
 	
 	
