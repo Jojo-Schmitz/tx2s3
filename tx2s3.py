9a8aa33c2d4bba5001b8853e355152ed5bc5cc2b
lasconic <lasconic@gmail.com>
2014-08-17 18:10:08 +0200

    zip mscore and instruments files

--- tx2s3.py
+++ tx2s3.py
@@ -1,53 +1,59 @@
 import glob, subprocess
 import os, sys, io
 import time, hashlib, json
+import zipfile
 
 #needs to be equal or smaller than the cron
 period=300
 outputDir = "share/locale/"
-s3Url = "s3://extensions.musescore.org/languages/"
+s3Url = "s3://extensions.musescore.org/2.0/languages/"
 
 def processTsFile(prefix, langCode, data):
 	filename = prefix + '_' + lang_code
 	tsFilePath = outputDir + filename + ".ts"
 	qmFilePath = outputDir + filename + ".qm"
-		
-	# generate qm file
-	lrelease = subprocess.Popen(['lrelease', tsFilePath, '-qm', qmFilePath])
-	lrelease.communicate()
-	
-	# get qm file size
-	file_size = os.path.getsize(qmFilePath)
-	file_size = "%.2f" % (float(file_size) / 1024)
-	
-	#compute Qm file hash
-	file = open(qmFilePath)
-	hash_file = hashlib.sha1()
-	hash_file.update(file.read())
-	
-	if lang_code not in data:
-		data[lang_code] = {}
-	#if prefix not in data[lang_code]
-	#	data[lang_code][prefix] = {}
 
-	data[lang_code]["file_name"] = filename + ".qm"
-	data[lang_code]["name"] = langCodeNameDict[lang_code]
-	data[lang_code]["hash"] = str(hash_file.hexdigest())
-	data[lang_code]["file_size"] = file_size
+	lang_time = int(os.path.getmtime(tsFilePath))
+	cur_time = int(time.time())
+	#print cur_time,lang_time,cur_time-lang_time
+	
+	# if the file has been updated, update or add entry in details.json
+	if cur_time - lang_time < period:
+		# generate qm file
+		lrelease = subprocess.Popen(['lrelease', tsFilePath, '-qm', qmFilePath])
+		lrelease.communicate()
+		
+		# get qm file size
+		file_size = os.path.getsize(qmFilePath)
+		file_size = "%.2f" % (float(file_size) / 1024)
+		
+		#compute Qm file hash
+		file = open(qmFilePath)
+		hash_file = hashlib.sha1()
+		hash_file.update(file.read())
+		file.close()
+		
+		if lang_code not in data:
+			data[lang_code] = {}
+		if prefix not in data[lang_code]:
+			data[lang_code][prefix] = {}
+
+		data[lang_code][prefix]["file_name"] = filename + ".qm"
+		data[lang_code][prefix]["hash"] = str(hash_file.hexdigest())
+		data[lang_code][prefix]["file_size"] = file_size
+
+		return True
+	else:
+		return False
 
 
 #pull ts files from transifex, use .tx/config
-pull = subprocess.Popen(['tx', '-q', 'pull', '-a'])
+pull = subprocess.Popen(['tx', '-q', 'pull', '-a', '-s'])
 pull.communicate()
 
 
-tsFilePaths = glob.glob(outputDir + "*.ts")
-if "share/locale/mscore_en.ts" in tsFilePaths:
-	tsFilePaths.remove("share/locale/mscore_en.ts")
-
 newDetailsFile = False
 translationChanged = False
-
 
 #read languages.json and store language code and name
 langCode_file = open("languages.json", "r+")
@@ -67,27 +73,52 @@ else:
 	data["version"] = "2.0"
 
 
+translationChanged = newDetailsFile
 for lang_code, languageName in langCodeNameDict.iteritems():
-	filename = 'mscore_' + lang_code
-	tsFilePath = outputDir + filename + ".ts"
-
-	lang_time = int(os.path.getmtime(tsFilePath))
-	cur_time = int(time.time())
-	#print cur_time,lang_time,cur_time-lang_time
 	
-	# if the file has been updated, update or add entry in details.json
-	if cur_time - lang_time < period or newDetailsFile:
-		processTsFile("mscore", lang_code, data)
-		push_s3 = subprocess.Popen(['s3cmd', 'put', '--acl-public', '--guess-mime-type', qmFilePath, s3Url + filename +'.qm'])
-		push_s3.communicate()
-		translationChanged = True
+	updateMscore = processTsFile("mscore", lang_code, data)
+	translationChanged = updateMscore or translationChanged
+	
+	updateInstruments = processTsFile("instruments", lang_code, data)
+	translationChanged = updateInstruments or translationChanged
+
+	if (updateMscore or updateInstruments):
+		#create a zip file, compute size, hash, add it to json and save to s3
+		zipName = 'locale_' + lang_code + '.zip'
+		zipPath = outputDir + zipName
+		myzip = zipfile.ZipFile(zipPath, mode='w')
+		qmFilePath = outputDir + 'mscore_' + lang_code + ".qm"
+		myzip.write(qmFilePath)
+		qmFilePath = outputDir + 'instruments_' + lang_code + ".qm"
+		myzip.write(qmFilePath)
+		myzip.close()
+
+		# get zip file size
+		file_size = os.path.getsize(zipPath)
+		file_size = "%.2f" % (float(file_size) / 1024)
+		
+		#compute zip file hash
+		file = open(zipPath)
+		hash_file = hashlib.sha1()
+		hash_file.update(file.read())
+		file.close()
+
+		data[lang_code]["file_name"] = zipName
+		data[lang_code]["name"] = langCodeNameDict[lang_code]
+		data[lang_code]["hash"] = str(hash_file.hexdigest())
+		data[lang_code]["file_size"] = file_size
+
+		push_zip = subprocess.Popen(['s3cmd','put','--acl-public', '--guess-mime-type', zipPath, s3Url + zipName])
+		push_zip.communicate()
+
+
 
 json_file = open(outputDir + "details.json", "w")
 json_file.write(json.dumps(data, sort_keys=True, indent=4))
 json_file.close()
 
 if translationChanged:
-	push_json=subprocess.Popen(['s3cmd','put','--acl-public', '--guess-mime-type', outputDir+'details.json', s3Url + 'details.json'])
+	push_json = subprocess.Popen(['s3cmd','put','--acl-public', '--guess-mime-type', outputDir + 'details.json', s3Url + 'details.json'])
 	push_json.communicate()
 	
 	
